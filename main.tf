3fd646a04e3b36015e605213f461cb0e29e9338e
vuongphamdev <phamnhuvuong96@gmail.com>
2025-08-25 22:44:27 +0700

    First commit

---
+++ main.tf
@@ -0,0 +1,33 @@
+resource "aws_iam_role" "lambda_role" {
+  name = "hello-lambda-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       = aws_iam_role.lambda_role.name
+}
+
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = "index.js"
+  output_path = "lambda.zip"
+}
+
+resource "aws_lambda_function" "hello_lambda" {
+  filename         = "lambda.zip"
+  function_name    = "hello-lambda"
+  role            = aws_iam_role.lambda_role.arn
+  handler         = "index.handler"
+  runtime         = "nodejs18.x"
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+}
